4b26a7cc8bcd17caec5a5e595bea815f098c7cde
NaveenGitHub12 <rnaveenkumar701@gmail.com>
2022-03-05 19:17:49 +0530

    abc12

---
+++ src/org/login/A.java
@@ -0,0 +1,11 @@
+package org.login;
+
+public class A {
+	static int f=10;
+	public void add() {
+		int b=15+f;
+	}
+}
+
+	
+
---
+++ src/org/login/B.java
@@ -0,0 +1,13 @@
+package org.login;
+
+public class B extends A  {
+	
+	private void difference() {
+
+	int	d=25+f;
+	}
+	
+	
+
+}
+
